d3c3fac15269a7fb94128244eb04307795853f3e
StanleeO <stanlee.okwii@gmail.com>
2022-06-01 00:13:47 +0300

    Set up frontend

---
+++ frontend/src/redux/actions/index.js
@@ -0,0 +1,7 @@
+// import { authentication } from './authentication'
+
+export const useActions = (state, dispatch) => {
+  return {
+    // authentication: authentication({state, dispatch}),
+  }
+};
---
+++ frontend/src/redux/reducers/authentication.js
@@ -0,0 +1,10 @@
+export const auth = {
+  user: {}
+}
+
+export const authReducer = (state, action) => {
+  switch (action.type) {
+    default:
+      throw new Error("Unexpected action");
+  }
+};
