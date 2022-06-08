cfeac7ea7248f9b1e4511b6f9a7f5035d443896b
StanleeO <stanlee.okwii@gmail.com>
2022-06-08 09:28:59 +0300

    Expire token after a default value of one hour

--- backend/api/config.py
+++ backend/api/config.py
@@ -9,4 +9,4 @@ class BaseConfig():
     SQLALCHEMY_TRACK_MODIFICATIONS = os.getenv("SQLALCHEMY_TRACK_MODIFICATIONS", False)
     SECRET_KEY = os.getenv("SECRET_KEY", "random_secret_key")
     JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY", "random_jwt_secret_key")
-    JWT_ACCESS_TOKEN_EXPIRES = os.getenv("JWT_ACCESS_TOKEN_EXPIRES", 3600)
+    JWT_ACCESS_TOKEN_EXPIRES = os.getenv("JWT_ACCESS_TOKEN_EXPIRES", 3600000)
